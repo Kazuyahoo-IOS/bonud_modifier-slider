0fec0cdc2a2514863b8dcd12c0c1688fd0988b6e
kazuyahooo <46344542+kazuyahooo@users.noreply.github.com>
2020-04-25 12:31:17 +0800

    Initial Commit

---
+++ bonud_modifier&slider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bonud_modifier&slider
+//
+//  Created by 王瑋 on 2020/4/25.
+//  Copyright © 2020 王瑋. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
